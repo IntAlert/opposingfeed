db09aaebdbef6232439f72004c98c929c50da33d
Alan Thomson <alan@projectmccoll.co.uk>
2016-11-17 17:06:53 +0100

    front page

--- app/public/javascripts/apps/frontend.js
+++ app/public/javascripts/apps/frontend.js
@@ -1,5 +1,5 @@
 var app = angular
-	.module('OpposingFeedPublic', ['ngMaterial', 'ngMessages'])
+	.module('OpposingFeedPublic', ['ngMaterial', 'ngMessages', 'duScroll'])
 	// .config(function($compileProvider, $locationProvider, $mdThemingProvider, $mdIconProvider){
 	//   $mdIconProvider
 	//     .defaultIconSet('/components/material-design-icons/iconfont/MaterialIcons-Regular.svg', 24)
@@ -10,3 +10,21 @@ var app = angular
  //          .accentPalette('orange')
  //          .warnPalette('red');
 	// });
+
+
+	.config(function($compileProvider, $locationProvider, $sceDelegateProvider){
+	   // Allow Whatsapp link
+		$compileProvider.aHrefSanitizationWhitelist(/^\s*(whatsapp|http|https|mailto):/);
+		
+		$locationProvider.html5Mode({
+			enabled: true,
+			rewriteLinks: false
+		});
+
+		$sceDelegateProvider.resourceUrlWhitelist([
+			// Allow same origin resource loads.
+			'self',
+			// Allow loading from FB
+			'https://www.facebook.com/**'
+		]); 
+});
--- app/public/javascripts/controllers/FrontendController.js
+++ app/public/javascripts/controllers/FrontendController.js
@@ -1,4 +1,68 @@
-app.controller('FrontendController', function ($scope, $window, $location, $anchorScroll) {
+app.controller('FrontendController', function ($scope, $document, $mdMedia) {
 
+
+
+
+	$scope.feed = {
+		embedOptions: {
+			width: $mdMedia('(min-width: 769px)') ? 600 : '100%',
+			height: $mdMedia('(min-width: 769px)') ? 300 : 360
+		},
+		received: false,
+		stories: [
+			{
+				url: "https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2F20531316728%2Fposts%2F10154009990506729%2F&show_text=true&appId=1716003962060301"
+			},
+			{
+				url: "https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2F20531316728%2Fposts%2F10154009990506729%2F&show_text=true&appId=1716003962060301"
+			},
+			{
+				url: "https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2F20531316728%2Fposts%2F10154009990506729%2F&show_text=true&appId=1716003962060301"
+			}
+		]
+	}
+
+	$scope.viewpoints = [
+		{
+			text: "Cheese is great"
+		},
+		{
+			text: "It's Six O'Clock"
+		},
+		{
+			text: "Liam's jeans are baggy"
+		}
+	];
+
+	$scope.currentViewPoint = 0
+
+
+	$scope.saveViewpointPosition = function(i, agree) {
+		$scope.currentViewPoint = i + 1
+	}
+
+
+
+
+
+
+
+
+	$scope.scrollToViewpoints = function() {
+		scrollTo('viewpoints')
+	}
+
+	$scope.scrollToFeed = function() {
+		$scope.feed.received = true;
+		scrollTo('feed')
+	}
+
+
+	var scrollTo = function(elementId){
+		var section = angular.element(document.getElementById(elementId));
+		var offset = 0;
+		var duration = 1000;
+	    $document.scrollToElement(section, offset, duration);
+	}
 
 })
